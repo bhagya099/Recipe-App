4877e5917c3f593ed29953ea3f4eb8aac4e53149
Bhagyashree <bhagya099@gmail.com>
2021-07-06 21:23:48 +1000

    center the delete button

--- js/recipe.js
+++ js/recipe.js
@@ -4,8 +4,6 @@ console.log("this is class.js file");
 const createRecipe = (id, recipeName, descriptions, ingredients) => {
     const html = `
    <div class="card" data-id="${id}">
-   
-        <img class="image" src="./images/background-image.jpg" alt="dish pic">
         <h3 class="name"><sapn>${recipeName}</sapn></h3>
         <p class="ingredients"> <sapn>Ingredients</sapn> <br>${ingredients}</p>
         <p class="descriptions"> <sapn>How to make</sapn> <br>${descriptions}</p>
